4d04781d63aabb558fa0a6bb7557db97fa5c8c28
Job Céspedes Ortiz <jobcespedes@gmail.com>
2021-06-26 14:17:27 -0600

    feat: add python code related to autoexpansion

--- plugins/filter/convert_bytes.py
+++
@@ -1,9 +0,0 @@
-class FilterModule(object):
-    def filters(self):
-        return {
-            'b_to_gib': self.b_to_gib,
-        }
-
-    def b_to_gib(self, bytes):
-        gib = round(bytes / (1024**3),1)
-        return gib
