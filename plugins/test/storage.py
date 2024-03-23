f457d80c9981e542c8d4197c3c8d54b1dec0b63d
Job Céspedes Ortiz <jobcespedes@gmail.com>
2024-03-23 13:34:49 -0600

    build: bump operator sdk to 1.33

--- plugins/test/storage.py
+++ plugins/test/storage.py
@@ -1,10 +1,15 @@
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
-from ansible_collections.krestomatio.k8s.plugins.module_utils.storage import below_twenty_pct
+from ansible_collections.krestomatio.k8s.plugins.module_utils.storage import (
+    below_twenty_pct,
+)
+
+
 
 
 class TestModule:
 
     def tests(self):
-        return {'below_twenty_pct': below_twenty_pct}
+        return {"below_twenty_pct": below_twenty_pct}
